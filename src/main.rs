812436afeddd64d5b1efbf46f87f39b74fdd2bf9
Nicola Musatti <nicola.musatti@gmail.com>
2021-09-07 20:08:44 +0200

    Implement basic routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, routes, serde::{Serialize, json::Json}};
+
+
+#[derive(Serialize)]
+struct Pokemon<'p> {
+    name: &'p str,
+}
+
+#[get("/<name>")]
+fn plain(name: &str) -> Json<Pokemon> {
+    Json(Pokemon{ name })
+}
+
+#[get("/translated/<name>")]
+fn translated(name: &str) -> Json<Pokemon> {
+    Json(Pokemon{ name })
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    Ok(rocket::build().mount("/pokemon", routes![plain, translated]).launch().await?)
 }
