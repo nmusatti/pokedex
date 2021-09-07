358baf483d7f0876a9b193b6b2e4b20726759b12
Nicola Musatti <nicola.musatti@gmail.com>
2021-09-08 00:01:29 +0200

    Complete tracer bullet

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
-use rocket::{get, routes, serde::{Serialize, json::Json}};
-
+use rocket::{
+    get, routes,
+    serde::{json::Json, Serialize},
+    Error,
+};
 
 #[derive(Serialize)]
-struct Pokemon<'p> {
-    name: &'p str,
+struct Pokemon {
+    name: String,
+}
+
+async fn pokemon(name: &str) -> Result<Pokemon,reqwest::Error> {
+    let resp = reqwest::get(format!("https://pokeapi.co/api/v2/pokemon/{}", name))
+    .await?
+    .json::<serde_json::Value>()
+    .await?;
+
+    Ok(Pokemon{ name: resp["name"].to_string() })
 }
 
 #[get("/<name>")]
-fn plain(name: &str) -> Json<Pokemon> {
-    Json(Pokemon{ name })
+async fn plain(name: &str) -> Json<Pokemon> {
+    Json(pokemon(name).await.unwrap())
 }
 
 #[get("/translated/<name>")]
-fn translated(name: &str) -> Json<Pokemon> {
-    Json(Pokemon{ name })
+async fn translated(name: &str) -> Json<Pokemon> {
+    Json(pokemon(name).await.unwrap())
 }
 
 #[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
-    Ok(rocket::build().mount("/pokemon", routes![plain, translated]).launch().await?)
+async fn main() -> Result<(), Error> {
+    Ok(rocket::build()
+        .mount("/pokemon", routes![plain, translated])
+        .launch()
+        .await?)
 }
